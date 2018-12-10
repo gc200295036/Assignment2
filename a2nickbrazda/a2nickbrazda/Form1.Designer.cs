3b1a74e64946f302c231a957803132f460c8bcf9
gc200295036 <44586823+gc200295036@users.noreply.github.com>
2018-12-10 00:33:52 -0500

    Add files via upload
    A2 Meal Ordering Form 
    (First Commit)

---
+++ a2nickbrazda/a2nickbrazda/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace a2nickbrazda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrderNow = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTaxes = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOrderNow
+            // 
+            this.btnOrderNow.BackColor = System.Drawing.SystemColors.Window;
+            this.btnOrderNow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrderNow.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrderNow.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnOrderNow.Location = new System.Drawing.Point(90, 106);
+            this.btnOrderNow.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnOrderNow.Name = "btnOrderNow";
+            this.btnOrderNow.Size = new System.Drawing.Size(88, 22);
+            this.btnOrderNow.TabIndex = 0;
+            this.btnOrderNow.Text = "Order Now";
+            this.btnOrderNow.UseVisualStyleBackColor = false;
+            this.btnOrderNow.Click += new System.EventHandler(this.btnOrderNow_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bagel",
+            "Vegetarian Special",
+            "Protein Platter"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 23);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Bagel",
+            "Vegetarian Special",
+            "Protein Platter"});
+            this.comboBox2.Location = new System.Drawing.Point(71, 48);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(140, 20);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Bagel",
+            "Vegetarian Special",
+            "Protein Platter"});
+            this.comboBox3.Location = new System.Drawing.Point(71, 73);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(140, 20);
+            this.comboBox3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(256, 48);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(256, 74);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(116, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(329, 103);
+            this.lblSubtotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(12, 12);
+            this.lblSubtotal.TabIndex = 7;
+            this.lblSubtotal.Text = "-";
+            // 
+            // lblTaxes
+            // 
+            this.lblTaxes.AutoSize = true;
+            this.lblTaxes.Location = new System.Drawing.Point(329, 118);
+            this.lblTaxes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTaxes.Name = "lblTaxes";
+            this.lblTaxes.Size = new System.Drawing.Size(12, 12);
+            this.lblTaxes.TabIndex = 8;
+            this.lblTaxes.Text = "-";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(329, 133);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(12, 12);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 10);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Breakfast:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 10);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Lunch:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 10);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Dinner:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 8);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Meals:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Quantity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(254, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 9);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Subtotal:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(260, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 9);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Taxes:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Sans Typewriter", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(260, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 9);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Total:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(310, 103);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(12, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "$";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(310, 118);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(12, 12);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "$";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(310, 133);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(12, 12);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(390, 148);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTaxes);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnOrderNow);
+            this.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.Text = "A2NICKBRAZDA-PROGRAMMING FUND.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrderNow;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTaxes;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ a2nickbrazda/a2nickbrazda/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a2nickbrazda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Meal Ordering Form: Created By Nicolas Brazda ~ Last Updated on Sat-December-08/2018
+        private void btnOrderNow_Click(object sender, EventArgs e)
+        {
+            //array "price" (bagel = $3.95, Vegetarian Special = 10.95$ Protein Platter 11.95$)
+            double[] price = { 3.95, 10.95, 11.95 };
+
+            int item = comboBox1.SelectedIndex;
+            int item2 = comboBox2.SelectedIndex;
+            int item3 = comboBox3.SelectedIndex;
+            double quantity1;
+            double quantity2;
+            double quantity3;
+
+            if (comboBox1.SelectedIndex != -1)
+            {
+                bool isNum = double.TryParse(textBox1.Text, out quantity1);
+                if (isNum && quantity1 > 0)
+                {
+
+                }
+
+                if (comboBox2.SelectedIndex != -1)
+                {
+                    bool isNum2 = double.TryParse(textBox2.Text, out quantity2);
+                    if (isNum2 && quantity2 > 0)
+                        if (comboBox3.SelectedIndex != -1)
+                        {
+                            bool isNum3 = double.TryParse(textBox3.Text, out quantity3);
+                            if (isNum3 && quantity3 > 0)
+                            {   //0.13 cent tax
+                                double Tax = 0.13;
+                                // calculate subtotal
+                                double subtotal = price[item] * quantity1 + price[item2] * quantity2 + price[item3] * quantity3;
+                                // subtotal * 0.13 (salesTaxRate)
+                                double salesTax = subtotal * Tax;
+                                // add subtotal + taxes together for Total cost
+                                double Total = subtotal + salesTax;
+                                //display calculations to label
+                                lblTaxes.Text = Convert.ToString(salesTax);
+                                lblTotal.Text = Convert.ToString(Total);
+                                lblSubtotal.Text = Convert.ToString(subtotal);
+                                // Display Invalid labels for special characters in Textboxes
+
+
+
+                            }
+                        }
+                }
+            }
+
+        }
+        // Make Textboxes Display an Error Message When typing in special characters:
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        { 
+         try
+        {
+         int i = Convert.ToInt32(textBox1.Text.Trim());
+        }
+        catch
+        {
+         MessageBox.Show("You Must Enter a Valid Number");
+                }
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        {
+        try
+        {
+        int i = Convert.ToInt32(textBox1.Text.Trim());
+        }
+        catch
+        {
+        MessageBox.Show("You Must Enter a Valid Number");
+                    }
+                }
+            }
+
+            private void textBox3_TextChanged(object sender, EventArgs e)
+            {
+                {
+                    try
+                    {
+                        int i = Convert.ToInt32(textBox1.Text.Trim());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("You Must Enter a Valid Number");
+                    }
+                }
+            }
+        }
+    }
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
